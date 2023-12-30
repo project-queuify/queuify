4a4b7b7ba9a79b415b1e9a13c2cfa8221c47c11b
Mr.Online <mronlinesworld@gmail.com>
2023-12-30 17:29:27 +0530

    chore: update example with both queue

--- example/simple-queue/queues/first-queue.ts
+++ example/simple-queue/queues/first-queue.ts
@@ -1,3 +1,6 @@
 import Queue from '../../../lib';
 
 new Queue('first-queue', 'redis://127.0.0.1:6379', { connectTimeout: 5000 });
+console.log('Redis queue connected');
+new Queue('first-queue', 'redis://127.0.0.1:6380', { connectTimeout: 5000 });
+console.log('Dragonfly queue connected');
